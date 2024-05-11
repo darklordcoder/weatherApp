d9868ace19ff46a31b0b5aca6a71448714440651
Salvador M Gomez <darklord@sin-nombre.com>
2024-05-11 16:06:49 -0600

    start

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-svg";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={tw`flex-1 bg-neutral-800`}>
+        <SafeAreaView>
+          <ExpoStatusBar style="light" />
+          <View>
+            <Text className="text-neutral-400">PROBANDO ESTA APP CHONCHA</Text>
+          </View>
+        </SafeAreaView>
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
